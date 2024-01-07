a3ed576dd7e661db85080d13a45803eed26b136e
CocoJL <jurgenlohu1@gmail.com>
2024-01-07 22:14:01 +0200

    edited html and css, 95% layout done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
-const clear = document.querySelector(".clear");
+const buttons = document.querySelectorAll(".number");
 const display = document.querySelector("#display");
+const operator = document.querySelectorAll(".operation");
+const clear = document.querySelector("clear");
+const equal = document.querySelector("equal");
